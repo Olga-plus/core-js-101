a56ded5dbd68b1932adf92d0724b070236e8bc30
Olga Klokova <olga-plus@gmail.com>
2021-06-03 21:52:47 +0200

    next

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -530,7 +530,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  return arr.flat().map(childrenSelector);
+
+  return
 }
 
 
