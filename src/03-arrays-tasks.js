40f6eceb07333b87a020c6b31857ce7be01db4c1
Olga Klokova <olga-plus@gmail.com>
2021-06-03 21:58:28 +0200

    fix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -530,8 +530,7 @@ function group(/* array, keySelector, valueSelector */) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-
-  return
+  return arr.flat().map(childrenSelector);
 }
 
 
--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -51,8 +51,8 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(proto, json) {
-  return JSON.parse(json);
+function fromJSON(/* proto, json */) {
+  throw new Error('Not implemented');
 }
 
 
